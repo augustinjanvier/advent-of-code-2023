ce0d60455e75cf4ab3fc8be5cc01d234eaffb3d4
augustinjanvier <88078818+augustinjanvier@users.noreply.github.com>
2023-12-05 17:14:49 +0100

    Part 1 done

--- day_5/day_5_part1.py
+++ day_5/day_5_part1.py
@@ -1,11 +1,12 @@
 # https://adventofcode.com/2023/day/5
 
-file_name = "input_small.txt"
+file_name = "input.txt"
 file = open(file_name, "r")
 
 seeds = file.readline().strip().split(": ")[1].split(" ")
+seeds = list(map(int, seeds))
 file.readline()
-map = []
+maps = []
 
 current_map = []
 
@@ -13,19 +14,43 @@ current_map = []
 for line in file:
     line = line.strip()
     if line == "":
-        print("space")
-        map.append(current_map)
+        current_map = sorted(current_map)
+        maps.append(current_map)
         continue
     
     line = line.split(" ")
 
     if line[1] == "map:":
         current_map = []
-        print("map line")
         continue
-    
+    line = list(map(int, line))
     current_map.append(line)
 
-# Now we need to go through the whole table, going from the seed to find the location
+current_map = sorted(current_map)
+maps.append(current_map)
 
+# Now we need to go through the whole table, going from the seed to find the location
+def get_location_from_seed(seed, maps):
+    source = seed
+    for map in maps:
+        destination = get_destination_from_source(source, map)
+        source = destination
+    return source
+
+def get_destination_from_source(source, map):
+    destination = source
+    for combinaison in map:
+        if source in range(combinaison[1],combinaison[1] + combinaison[2]):
+            difference = destination - combinaison[1]
+            destination = combinaison[0] + difference
+            break
+    return destination
+
+locations = []
+
+for seed in seeds:
+    locations.append(get_location_from_seed(seed, maps))
+
+locations = sorted(locations)
+print(locations[0])
     
