77ff0aaddac2025f894f99f7d9b71149461a92b3
augustinjanvier <aug.j22@hotmail.fr>
2023-12-10 12:08:50 +0100

    day 10 part 2

--- day_10/day_10_part_2.py
+++ day_10/day_10_part_2.py
@@ -15,14 +15,15 @@ for line in file:
         x = pipes_table.index(line)
         y = line.index("S")
 
-pipes_way_table = []
-for line in pipes_table:
-    tmp = []
-    for c in line:
-        tmp.append(" ")
-    pipes_way_table.append(tmp)   
+# pipes_way_table = []
+# for line in pipes_table:
+#     tmp = []
+#     for c in line:
+#         tmp.append(" ")
+#     pipes_way_table.append(tmp)   
 
 loop_complete = False
+polygone = [(x,y)]
 
 def get_next_pipe(pipex, pipey, where_from, table):
     match table[pipex][pipey]:
@@ -72,11 +73,13 @@ def get_next_pipe(pipex, pipey, where_from, table):
                 return (x, y - 1, 'right')
 
 where_from = 'none'
+
 # │ ┌ ┐ └ ┘ ─
 
 while not loop_complete:
-    pipes_way_table[x][y]=pipes_table[x][y]
-    pipes_way_table[x][y] = pipes_way_table[x][y].replace("|", "│").replace("F","┌").replace("L","└").replace("7","┐").replace("J","┘").replace("-","─")
+    polygone.append([x,y])
+    #pipes_way_table[x][y]=pipes_table[x][y]
+    #pipes_way_table[x][y] = pipes_way_table[x][y].replace("|", "│").replace("F","┌").replace("L","└").replace("7","┐").replace("J","┘").replace("-","─")
 
     pipe = get_next_pipe(x, y, where_from, pipes_table)
     x, y, where_from = pipe[0], pipe[1], pipe[2]
@@ -84,10 +87,24 @@ while not loop_complete:
     if pipes_table[x][y] == "S":
         loop_complete = True
 
-for line in pipes_way_table:
-    tmp = ""
-    for c in line:
-        if c == " ":
-            c = "."
-        tmp += c
-    print(tmp)
+# for line in pipes_way_table:
+#     tmp = ""
+#     for c in line:
+#         if c == " ":
+#             c = "."
+#         tmp += c
+#     print(tmp)
+
+from matplotlib.path import Path
+
+result = 0
+
+p = Path(polygone)
+for y in range(len(pipes_table)):
+    for x in range(len(pipes_table[0])):
+        if [x,y] in polygone:
+            continue
+        if p.contains_point((x,y)):
+            result += 1
+
+print(result)
